0b5826b458f6abb47a1119cefec852da47942aab
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2024-04-15 19:12:28 -0400

    localPath added to config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
     i18n: {
         locales: ['es', 'en'],
         defaultLocale: 'es',
     },
+    localePath: path.resolve('./public/locales')
 }
