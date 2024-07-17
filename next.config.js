615f017007b623bfbbf4cfe92337913529bf22b0
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2024-07-17 13:44:37 -0400

    x robots tags noindex added to public files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 const {i18n} = require("./next-i18next.config");
 
 module.exports = {
+    async headers() {
+      return [
+        {
+        source: '/locales/:path*',
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex'
+            }
+          ],
+        },
+        {
+        source: '/:path*.(gif|jpg|jpeg|svg|pdf|png|ico|html|glb)',
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex'
+            }
+          ],
+        },
+      ]
+    },
     i18n
 }
